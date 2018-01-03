c684fa836be9d079b52328a9d4cdda535f7488f0
iqbalsalman <iqbalsalman1707@gmail.com>
2018-01-03 14:47:44 +0700

    perbaikan Lingkaran

--- Lingkaran.java
+++ Lingkaran.java
@@ -3,10 +3,10 @@ class Lingkaran{
 	public static void main(String[] args){
 		// System.out.println("Nilai PT "+ Math.PI);
 
-		 Integer  jarijari = 15;
-		  Integer  diameter = jarijari*2;
+		
+		 Integer   diameter = Variabel.jarijari*2;
 		  Double    keliling =Math.PI*diameter;
-		System.out.println("Nilai PI "+keliling+ " Maka Nilai Keliling "+diameter);
+		System.out.println("Nilai PI "+Math.PI+ " Maka Nilai Keliling "+keliling);
 
 
 	}
---
+++ Variabel.java
@@ -0,0 +1,4 @@
+class Variabel{
+
+	 public static final Integer   jarijari = 15;
+}
